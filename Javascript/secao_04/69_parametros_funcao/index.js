ee7575eb34ce9bf0a8de7a13dfa414e33381fca9
IvanRafael-Dev <ivanrafael.dev@gmail.com>
2021-08-24 20:18:01 -0300

    parametros de funcao

--- Javascript/secao_04/69_parametros_funcao/index.js
+++ Javascript/secao_04/69_parametros_funcao/index.js
@@ -16,3 +16,65 @@ function sumValues() {
 }
 
 sumValues(1, 2, 3, 4, 5);
+
+function funcao2(a, b, c, d, e, f) {
+  console.log(a, b, c, d, e, f); // se eu nao enviaar valor, o padrao eh undefined
+}
+
+funcao2(1, 2, 3);
+
+// default params
+function sum(a, b = 1, c = 1) {
+  const total = a + b + c;
+  console.log(total);
+}
+
+sum(2);
+sum(2, 2);
+sum(2, '', 2)
+
+// destructuring parameters
+function nomeCompleto({ nome, sobrenome }) {
+  console.log(nome, sobrenome);
+}
+
+nomeCompleto({ nome: 'Ivan', sobrenome: 'Rafael'});
+
+const obj = {
+  nome: 'Paul',
+  sobrenome: 'Stamits',
+}
+
+nomeCompleto(obj);
+
+function logArrayValues([value1, value2, value3]) {
+  console.log(value1, value2, value3);
+};
+
+const array = ['Valores', 'do', 'array'];
+
+logArrayValues(array);
+
+// rest operator, ultimo parametro da funcao
+function conta(operador, acumulador, ...numeros) {
+  for (let num of numeros) {
+    if (operador === '+') {
+      acumulador += num;
+    } else if (operador === '-') {
+      acumulador -= num;      
+    } else if (operador === '*') {
+      acumulador *= num;
+    } else if (operador === '/') {
+      acumulador /= num;
+    }
+  }
+  console.log(acumulador);
+}
+
+conta('/', 1, 10, 20, 30, 40, 50);
+
+const showArgs = (...args) => {
+  console.log(args);
+};
+
+showArgs('a', 'b', 'c')
