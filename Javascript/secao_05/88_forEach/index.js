f41e233c5e3cc0efb6795117bb9e6bbae6fc81ce
IvanRafael-Dev <ivanrafael.dev@gmail.com>
2021-08-30 12:51:05 -0300

    forEach

---
+++ Javascript/secao_05/88_forEach/index.js
@@ -0,0 +1,7 @@
+// metodo de arrays
+
+const array1 = [12, 12, 33, 44, 535, 36, 57, 8, 49]
+
+for (let valor of array1) console.log(valor);
+
+array1.forEach((valor) => console.log(`num: ${valor}`));
