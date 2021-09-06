cc217a3b45ac8cbed15f9fc3d467b7fbc6e0ce17
IvanRafael-Dev <ivanrafael.dev@gmail.com>
2021-09-06 12:12:58 -0300

    setter de nomeCompleto

--- Javascript/secao_07/103_setters_getters/other.js
+++ Javascript/secao_07/103_setters_getters/other.js
@@ -1,13 +1,22 @@
 class Pessoa {
   constructor(nome, sobrenome) {
     this.nome = nome;
-    this.somenome = sobrenome;
+    this.sobrenome = sobrenome;
   }
 
   get nomeCompleto() {
-    return `${this.nome} ${this.somenome}`
+    return `${this.nome} ${this.sobrenome}`
+  }
+
+  set nomeCompleto(valor) {
+    const valorArray = valor.split(' ');
+    this.nome = valorArray.shift();
+    this.sobrenome = valorArray.join(' ');
   }
 }
 
 const ivan = new Pessoa('Ivan', 'Rafael')
 console.log(ivan.nomeCompleto);
+
+ivan.nomeCompleto = 'Ivan Rafael Bonaparte'
+console.log(ivan);
