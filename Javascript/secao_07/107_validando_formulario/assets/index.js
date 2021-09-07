85911ee2b9fd538f4644484cc066244a016b9e57
IvanRafael-Dev <ivanrafael.dev@gmail.com>
2021-09-06 21:09:48 -0300

    cria classe ValidaFormulario

--- Javascript/secao_07/107_validando_formulario/assets/index.js
+++ Javascript/secao_07/107_validando_formulario/assets/index.js
@@ -1,1 +1,14 @@
-console.log('tudo ok');
+class ValidaFormulario {
+  constructor() {
+    this.formulario = document.querySelector('.formulario');
+    this.eventos();
+  }
+
+  eventos() {
+    this.formulario.addEventListener('click', (e) => {
+      
+    });
+  }
+}
+
+const validaForm = new ValidaFormulario();
