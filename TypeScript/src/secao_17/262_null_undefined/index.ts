5c3a5a22cf395a1e2390a5d3cbbfa1fba6e5826c
IvanRafael-Dev <ivanrafael.dev@gmail.com>
2021-10-06 22:17:17 -0300

    aula 262: type null e undefined

---
+++ TypeScript/src/secao_17/262_null_undefined/index.ts
@@ -0,0 +1,29 @@
+let x;
+
+if (typeof x === 'undefined') x = 20;
+
+console.log(x * 2);
+
+export function createPerson(
+  firstName: string,
+  lastName?: string, // opcional
+): {
+  firstName: string;
+  lastName?: string; // opcional
+} {
+  return { firstName, lastName };
+}
+
+export function squareOf(x: any): number | null {
+  // : number | null
+  if (typeof x === 'number') return x * x;
+  return null;
+}
+
+const squareOfTwoNumber = squareOf(2);
+
+if (squareOfTwoNumber === null) {
+  console.log('conta inválida', squareOfTwoNumber * 2); // reconhece o erro
+} else {
+  console.log(squareOfTwoNumber * 2); // afunilamento, ja entente que é um number
+}
