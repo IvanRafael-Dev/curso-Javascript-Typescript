de453f1d783500d6a43ed2bca39fa585c433203d
IvanRafael-Dev <ivanrafael.dev@gmail.com>
2021-10-11 21:08:22 -0300

    aula 272: type ass

---
+++ TypeScript/src/secao_17/272_type_assertions/index.ts
@@ -0,0 +1,13 @@
+// condicional
+const body = document.querySelector('body');
+if (body) body.style.backgroundColor = 'red'; // estritamento de tipo
+
+// Non-null assertion(!) --> nao recomendado
+// const body2 = document.querySelector('body')!;
+// body2.style.backgroundColor = 'red';
+
+// Type assertion
+const body3 = document.querySelector('body') as HTMLBodyElement;
+body3.style.backgroundColor = 'red';
+
+console.log(body3);
